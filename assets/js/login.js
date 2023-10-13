60b881e2c644d2f188b6248f2f472c6b11f22f16
Filip Gynthersen <filipg@live.dk>
2023-10-13 11:50:57 +0200

    Login fix2

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,42 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-    const emailInput = document.getElementById("email");
-    const passwordInput = document.getElementById("password");
+    const emailInput = document.getElementById('emailInput'); // Replace with the actual element
+    const passwordInput = document.getElementById('passwordInput'); // Replace with the actual element
+    const errorDiv = document.getElementById('errorDiv'); // Replace with the actual element
 
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
+    const formData = new URLSearchParams();
+    formData.append('email', emailInput.value);
+    formData.append('password', passwordInput.value);
 
-        const errorDiv = document.getElementById("error-message");
-        errorDiv.innerText = "";
-
-        if (emailInput.value.trim() === "" || passwordInput.value.trim() === "") {
-            errorDiv.innerText = "Udfyld alle felter.";
-            return;
-        }
-
-        // Check for "@" in the email
-        if (!emailInput.value.includes("@")) {
-            errorDiv.innerText = "Ugyldig Email.";
-            return;
-        }
-
-        fetch("https://adventurexpwebapp.azurewebsites.net/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: `email=${emailInput.value}&password=${passwordInput.value}`,
+    fetch('https://adventurexpwebapp.azurewebsites.net/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: formData,
+    })
+        .then((response) => {
+            if (response.status === 200) {
+                return response.text(); // Assuming your server sends a plain text response
+            } else if (response.status === 401) {
+                throw new Error('Unauthorized');
+            } else {
+                throw new Error('Failed to log in');
+            }
         })
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.auth) {
-                    window.location.href = "/dash";
-                } else {
-                    errorDiv.innerText = "Ugyldigt login. Prøv igen.";
-                }
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
-    });
-});
+        .then((data) => {
+            // Handle the response here
+            if (data === 'Success') {
+                // Successful login
+                window.location.href = '/dash.html';
+            } else {
+                errorDiv.innerText = 'Ugyldigt login. Prøv igen.';
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+})
